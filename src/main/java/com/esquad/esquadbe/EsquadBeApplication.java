ec53e84ed5db57a673786d9ed3ac588eca6a8444
kimmin1kk <rudals1888@naver.com>
2024-08-15 16:10:43 +0900

    initial commit

---
+++ src/main/java/com/esquad/esquadbe/EsquadBeApplication.java
@@ -0,0 +1,13 @@
+package com.esquad.esquadbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsquadBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsquadBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esquad/esquadbe/EsquadBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esquad.esquadbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsquadBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
