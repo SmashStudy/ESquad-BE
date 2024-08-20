6ee117ce36e5ed0eafc3eb34b037cbcf9e423045
yejeii <98299007+yejeii@users.noreply.github.com>
2024-08-20 11:58:47 +0900

    init: StudyRemind 엔티티 설정 초기화

---
+++ src/main/java/com/esquad/esquadbe/notification/entity/AlertDayType.java
@@ -0,0 +1,11 @@
+package com.esquad.esquadbe.notification.entity;
+
+public enum AlertDayType {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ src/main/java/com/esquad/esquadbe/studypage/entity/StudyRemind.java
@@ -0,0 +1,44 @@
+package com.esquad.esquadbe.studypage.entity;
+
+import com.esquad.esquadbe.global.entity.BasicEntity;
+import com.esquad.esquadbe.notification.entity.AlertDayType;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.context.annotation.Description;
+
+import java.time.LocalTime;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "STUDY_REMIND")
+@Description("스터디 리마인드")
+public class StudyRemind extends BasicEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "STUDY_PAGE_ID")
+    private StudyPage studyPage;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "DAY_AT", nullable = false)
+    private AlertDayType dayType;
+
+    @Column(name = "TIME_AT")
+    private LocalTime timeAt;
+
+    @Column(columnDefinition = "VARCHAR(100)")
+    private String description;
+
+    @Column(name = "REMIND_FLAG", columnDefinition = "BOOLEAN DEFAULT TRUE")
+    private boolean remindFlag = true;
+
+}
