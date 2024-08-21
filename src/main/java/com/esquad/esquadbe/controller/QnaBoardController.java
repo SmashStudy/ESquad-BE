5343f68df4a0975d94d808f08f04137508c9cf0a
wnsgud0310 <ffjjo0310@naver.com>
2024-08-22 00:01:36 +0900

    feat: 특정 게시글 목록 조회 기능 구현

--- src/main/java/com/esquad/esquadbe/controller/QnaBoardController.java
+++ src/main/java/com/esquad/esquadbe/controller/QnaBoardController.java
@@ -1,9 +1,11 @@
 package com.esquad.esquadbe.controller;
 
+import com.esquad.esquadbe.exception.ResourceNotFoundException;
 import com.esquad.esquadbe.qnaboard.entity.BookQnaBoard;
 import com.esquad.esquadbe.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,14 +16,19 @@ import java.util.List;
 public class QnaBoardController {
 
     private final QuestionRepository questionRepository;
-
+    //전체 게시글 조회
     @GetMapping("/qna/questions")
     public List<BookQnaBoard> getAllQuestions() {
         List<BookQnaBoard> bookQnaBoards = new ArrayList<>();
         for(BookQnaBoard question : questionRepository.findAll()) {
             bookQnaBoards.add(question);
         }
-
         return bookQnaBoards;
     }
+    @GetMapping("/qna/questions/{id}")
+    public BookQnaBoard getQuestionById(@PathVariable Long id) {
+        return questionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("id를 찾을수가 없습니다. " + id));
+    }
+
 }
