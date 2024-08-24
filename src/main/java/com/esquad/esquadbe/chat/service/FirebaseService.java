18ab76f1f5f0789f832b7ca970c53220b890bba9
0237hh <153145663+0237hh@users.noreply.github.com>
2024-08-24 14:04:50 +0900

    feat: 메세지 업데이트 기능 구현

--- src/main/java/com/esquad/esquadbe/chat/service/FirebaseService.java
+++ src/main/java/com/esquad/esquadbe/chat/service/FirebaseService.java
@@ -1,10 +1,10 @@
 package com.esquad.esquadbe.chat.service;
 
+import com.esquad.esquadbe.firebaseCallback.FirebaseCallback;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.*;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -41,7 +41,34 @@ public class FirebaseService {
             System.err.println("Firebase initialization error: " + e.getMessage());
         }
     }
+
     public DatabaseReference getReference(String path) {
         return firebaseDatabase.getReference(path);
+    }
+
+    public void updateMessage(String roomId, String messageId, String userId, String newMessage, FirebaseCallback callback) {
+        DatabaseReference messageRef = getReference("MESSAGES/" + roomId + "/" + messageId);
+        messageRef.child("userId").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String originalUserId = dataSnapshot.getValue(String.class);
+
+                if (originalUserId != null && originalUserId.equals(userId)) {
+                    messageRef.child("message").setValue(newMessage, (databaseError, databaseReference) -> {
+                        if (databaseError != null) {
+                            callback.onFailure(databaseError.toException());
+                        } else {
+                            callback.onSuccess();
+                        }
+                    });
+                } else {
+                    callback.onFailure(new Exception("Unauthorized"));
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                callback.onFailure(databaseError.toException());
+            }
+        });
     }
 }
