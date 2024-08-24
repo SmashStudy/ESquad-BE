fbbd974adcf79b665a3acf19bdb453ed70679ed7
wnsgud0310 <ffjjo0310@naver.com>
2024-08-24 20:48:10 +0900

    add: QnaBoardResponseDTO 클래스 생성

--- src/main/java/com/esquad/esquadbe/qnaboard/dto/QnaBoardResponseDTO.java
+++ src/main/java/com/esquad/esquadbe/qnaboard/dto/QnaBoardResponseDTO.java
@@ -1,9 +1,18 @@
 package com.esquad.esquadbe.qnaboard.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@NoArgsConstructor
+@Setter
 public class QnaBoardResponseDTO {
+    private Long id;
+    private String writerName;
+    private Long studyPageId;
+    private Long bookId;
+    private String title;
+    private String content;
+    private Integer likes;
+    private String createdAt;
+    private String updatedAt;
 }
