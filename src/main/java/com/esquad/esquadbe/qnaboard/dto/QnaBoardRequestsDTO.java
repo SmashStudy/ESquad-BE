264459ea86b54146faf48422b7388ace8b41d43b
wnsgud0310 <ffjjo0310@naver.com>
2024-08-25 15:46:58 +0900

    :mod: QnaBoardRequestsDTO 클래스에 정적 팩토리 메서드 추가

--- src/main/java/com/esquad/esquadbe/qnaboard/dto/QnaBoardRequestsDTO.java
+++ src/main/java/com/esquad/esquadbe/qnaboard/dto/QnaBoardRequestsDTO.java
@@ -1,6 +1,7 @@
 package com.esquad.esquadbe.qnaboard.dto;
 
 
+import com.esquad.esquadbe.qnaboard.entity.BookQnaBoard;
 import com.esquad.esquadbe.studypage.entity.Book;
 import com.esquad.esquadbe.studypage.entity.StudyPage;
 import com.esquad.esquadbe.user.entity.User;
@@ -17,4 +18,18 @@ public class QnaBoardRequestsDTO {
     private Book book;
     private String content;
     private Integer likes;
+
+    // 정적 팩토리 메서드 추가
+    public static QnaBoardRequestsDTO fromEntity(BookQnaBoard bookQnaBoard) {
+        return QnaBoardRequestsDTO.builder()
+                .id(bookQnaBoard.getId())
+                .writer(bookQnaBoard.getWriter())
+                .title(bookQnaBoard.getTitle())
+                .studyPage(bookQnaBoard.getStudyPage())
+                .book(bookQnaBoard.getBook())
+                .content(bookQnaBoard.getContent())
+                .likes(bookQnaBoard.getLikes())
+                .build();
+    }
+
 }
