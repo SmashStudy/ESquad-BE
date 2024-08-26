ef66a618628cd66f0f0088482a200b71553529b3
wive10 <70935978+wive10@users.noreply.github.com>
2024-08-27 02:31:30 +0900

    mod: controller 반환 값 변경

--- src/main/java/com/esquad/esquadbe/studypage/controller/BookController.java
+++ src/main/java/com/esquad/esquadbe/studypage/controller/BookController.java
@@ -1,12 +1,11 @@
 package com.esquad.esquadbe.studypage.controller;
 
 import com.esquad.esquadbe.studypage.service.BookService;
-import com.esquad.esquadbe.studypage.vo.BookDetailVo;
-import com.esquad.esquadbe.studypage.vo.BookVo;
+import com.esquad.esquadbe.studypage.dto.BookSearchDetailDto;
+import com.esquad.esquadbe.studypage.dto.BookSearchDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,19 +19,19 @@ public class BookController {
     private BookService bookService;
 
     @GetMapping("/search")
-    public ResponseEntity<List<BookVo>> searchTitle(@RequestParam("title") String title) {
+    public ResponseEntity<List<BookSearchDto>> searchTitle(@RequestParam("title") String title) {
         log.info("받은 제목: {}", title);
 
-        List<BookVo> bookList = bookService.resultList(title);
+        List<BookSearchDto> bookList = bookService.resultList(title);
 
         return ResponseEntity.status(200).body(bookList);
     }
 
     @GetMapping("/search/{isbn}")
-    public ResponseEntity<List<BookDetailVo>> showDetail(@PathVariable String isbn) {
+    public ResponseEntity<List<BookSearchDetailDto>> showDetail(@PathVariable String isbn) {
         log.info("받은 ISBN: {}", isbn);
 
-        List<BookDetailVo> book = bookService.resultDetail(isbn);
+        List<BookSearchDetailDto> book = bookService.resultDetail(isbn);
 
         return ResponseEntity.status(200).body(book);
     }
