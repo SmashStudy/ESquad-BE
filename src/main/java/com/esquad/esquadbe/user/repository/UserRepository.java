00fa4d26c8ea527d37d8713434c150aa575e1ed8
jeongmin <jeongmin0046@gmail.com>
2024-08-26 16:36:47 +0900

    refactor: CrudRepository로 리팩토링

--- src/main/java/com/esquad/esquadbe/user/repository/UserRepository.java
+++ src/main/java/com/esquad/esquadbe/user/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.esquad.esquadbe.user.repository;
 
 import com.esquad.esquadbe.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
     boolean existsByUserId(String userId);
     boolean existsByNickname(String nickname);
 
