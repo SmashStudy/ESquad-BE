e261e2c4a30f8ef99b51c089d0619206392c5b0b
yejeii <98299007+yejeii@users.noreply.github.com>
2024-09-02 21:17:46 +0900

    test: team 생성 테스트

--- src/test/java/com/esquad/esquadbe/team/controller/TeamRestControllerTest.java
+++ src/test/java/com/esquad/esquadbe/team/controller/TeamRestControllerTest.java
@@ -1,21 +1,50 @@
 package com.esquad.esquadbe.team.controller;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.esquad.esquadbe.team.service.TeamService;
 
 @SpringBootTest
-@Transactional
+@WebMvcTest(TeamRestController.class)
+@AutoConfigureMockMvc
 class TeamRestControllerTest {
 
-    @BeforeEach
-    void setUp() {
+    @Autowired
+    private MockMvc mockmvc;
+
+    @MockBean
+    private TeamService teamService;
+
+    @Test
+    @DisplayName("/team/new 로 접속하면 팀 스페이스를 생성하는 페이지가 나와야 한다.")
+    public void shoudReturnCreatePage() throws Exception {
+        
+        mockmvc.perform(
+            MockMvcRequestBuilders.get("/team/new"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("new")));
     }
 
     @Test
-    void checkTeamName() {
+    @DisplayName("팀 스페이스명의 유효성을 검사한다. DB에 있는 이름과 중복될 경우 실패해야 한다.")
+    public void shouldFailWhenExists() {
+        // given
+        final String givenName = "스메시";
+
+        // when
+        
+
+        // them
     }
 }
--- src/test/java/com/esquad/esquadbe/team/service/TeamServiceTest.java
+++ src/test/java/com/esquad/esquadbe/team/service/TeamServiceTest.java
@@ -1,9 +1,6 @@
 package com.esquad.esquadbe.team.service;
 
-import com.esquad.esquadbe.team.entity.TeamSpace;
-import com.esquad.esquadbe.team.repository.TeamRepository;
-import com.esquad.esquadbe.user.entity.User;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,26 +8,23 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
+import com.esquad.esquadbe.team.repository.TeamRepository;
 
 @ExtendWith(MockitoExtension.class)
 @Transactional
 class TeamServiceTest {
 
-    @InjectMocks
-    private TeamService teamService;
-
     @Mock
     private TeamRepository teamRepository;
 
-    @BeforeEach
-    void setUp() {
-
-        //TeamSpace teamSpace = TeamSpace.builder().teamName("스매시").
-
-    }
+    @InjectMocks
+    private TeamServiceImpl teamService;
 
     @Test
-    void checkTeamName() {
+    @DisplayName("중복 팀명 검색 시 예외 발생 ")
+    void shouldFailWhenNameExists() {
+        // final String givenName = "스메시";
+        // boolean expectedResult = teamService.checkTeamName(givenName);
+        // assertTrue(expectedResult);
     }
 }
