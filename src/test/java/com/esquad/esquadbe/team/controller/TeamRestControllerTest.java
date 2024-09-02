b717eb80858a5d4c1b6f8c0edc511d9a958295d2
yejeii <yesie0108@gmail.com>
2024-09-03 05:16:26 +0900

    test: 유효성 검사 테스트

--- src/test/java/com/esquad/esquadbe/team/controller/TeamRestControllerTest.java
+++ src/test/java/com/esquad/esquadbe/team/controller/TeamRestControllerTest.java
@@ -1,34 +1,48 @@
 package com.esquad.esquadbe.team.controller;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.esquad.esquadbe.team.service.TeamService;
+import com.esquad.esquadbe.global.handle.DuplicateNameException;
+import com.esquad.esquadbe.global.handle.ErrorCode;
+import com.esquad.esquadbe.team.repository.TeamRepository;
+import com.esquad.esquadbe.team.service.TeamServiceImpl;
 
 @SpringBootTest
-@WebMvcTest(TeamRestController.class)
 @AutoConfigureMockMvc
 class TeamRestControllerTest {
 
     @Autowired
     private MockMvc mockmvc;
 
-    @MockBean
-    private TeamService teamService;
+    @Mock
+    private TeamRepository teamRepository;
+
+    @InjectMocks
+    private TeamServiceImpl teamService;
+
 
     @Test
     @DisplayName("/team/new 로 접속하면 팀 스페이스를 생성하는 페이지가 나와야 한다.")
-    public void shoudReturnCreatePage() throws Exception {
+    public void shouldReturnCreatePage() throws Exception {
         
         mockmvc.perform(
             MockMvcRequestBuilders.get("/team/new"))
@@ -37,14 +51,40 @@ class TeamRestControllerTest {
     }
 
     @Test
-    @DisplayName("팀 스페이스명의 유효성을 검사한다. DB에 있는 이름과 중복될 경우 실패해야 한다.")
-    public void shouldFailWhenExists() {
+    @DisplayName("팀 스페이스명의 유효성을 검사한다. DB에 있는 이름과 중복될 경우 에러를 던진다.")
+    @ExceptionHandler(DuplicateNameException.class)
+    public void shouldOccurErrorWhenDuplicateName() throws Exception {
+
         // given
         final String givenName = "스메시";
+        given(teamService.verifyTeamName(givenName)).willThrow(new DuplicateNameException(ErrorCode.DUPLICATE_NAME));
 
-        // when
+        // when-then
+        MvcResult result = mockmvc.perform(get("/team/new/{teamName}", givenName)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print()) 
+            .andReturn();
+
+        int expected = result.getResponse().getStatus();
+        assertEquals(403, expected);
+    }
+
+    @Test
+    @DisplayName("팀 스페이스명의 유효성을 검사한다. 중복되지 않는 경우 200 상태코드를 보인다.")
+    public void shouldStatusIsOkWhenUniqueName() throws Exception {
+
+        final String givenName = "gilgili";
+        given(teamService.verifyTeamName(givenName)).willReturn(false);
+        int actual = HttpStatus.OK.value();
         
+        MvcResult result = mockmvc.perform(get("/team/new/{teamName}", givenName)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andReturn();
 
-        // them
+        int expected = result.getResponse().getStatus();
+        assertEquals(actual, expected);
     }
 }
--- src/test/java/com/esquad/esquadbe/team/service/TeamServiceTest.java
+++ src/test/java/com/esquad/esquadbe/team/service/TeamServiceTest.java
@@ -1,17 +1,23 @@
 package com.esquad.esquadbe.team.service;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.times;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.transaction.annotation.Transactional;
 
+import com.esquad.esquadbe.team.entity.TeamSpace;
 import com.esquad.esquadbe.team.repository.TeamRepository;
 
 @ExtendWith(MockitoExtension.class)
-@Transactional
 class TeamServiceTest {
 
     @Mock
@@ -20,11 +26,28 @@ class TeamServiceTest {
     @InjectMocks
     private TeamServiceImpl teamService;
 
-    @Test
-    @DisplayName("중복 팀명 검색 시 예외 발생 ")
-    void shouldFailWhenNameExists() {
-        // final String givenName = "스메시";
-        // boolean expectedResult = teamService.checkTeamName(givenName);
-        // assertTrue(expectedResult);
+    @BeforeEach
+    public void before() {
+        TeamSpace team = TeamSpace.builder()
+                    .teamName("blizard")
+                    .build();
+        lenient().when(teamRepository.save(any(TeamSpace.class))).thenReturn(team);
     }
+
+    @Test
+    @DisplayName("중복된 팀명으로 검색 테스트")
+    void shouldAssertTrueWhenDuplicateName() {
+
+        // given
+        final String givenName = "blizard";
+        given(teamRepository.existsByTeamName(givenName)).willReturn(true);
+
+        // when - then
+        boolean expectedResult = teamService.verifyTeamName(givenName);
+        assertTrue(expectedResult);
+
+        then(teamRepository).should(times(1)).existsByTeamName(givenName);
+    }
+
+    
 }
