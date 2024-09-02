249212c03f082fdcb75ac6ad7c82fdf201e58670
yejeii <yesie0108@gmail.com>
2024-09-03 05:15:32 +0900

    style: 메서드명 변경

--- src/main/java/com/esquad/esquadbe/team/service/TeamService.java
+++ src/main/java/com/esquad/esquadbe/team/service/TeamService.java
@@ -1,10 +1,6 @@
 package com.esquad.esquadbe.team.service;
 
-import com.esquad.esquadbe.team.entity.TeamSpace;
-
-import java.util.Optional;
-
 public interface TeamService {
 
-    public boolean checkTeamName(String teamName);
+    public boolean verifyTeamName(String teamName);
 }
--- src/main/java/com/esquad/esquadbe/team/service/TeamServiceImpl.java
+++ src/main/java/com/esquad/esquadbe/team/service/TeamServiceImpl.java
@@ -1,16 +1,12 @@
 package com.esquad.esquadbe.team.service;
 
-import com.esquad.esquadbe.team.controller.TeamRestController;
-import com.esquad.esquadbe.team.entity.TeamSpace;
-import com.esquad.esquadbe.team.repository.TeamRepository;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
-import java.util.Optional;
+import com.esquad.esquadbe.team.repository.TeamRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -21,10 +17,8 @@ public class TeamServiceImpl implements TeamService {
     private static Logger logger = LoggerFactory.getLogger(TeamServiceImpl.class);
 
     @Override
-    public boolean checkTeamName(String name) {
-
-        logger.info("checkTeamName()");
-
+    public boolean verifyTeamName(String name) {
+        logger.info("verifyTeamName()");
         return teamRepository.existsByTeamName(name);
     }
 }
