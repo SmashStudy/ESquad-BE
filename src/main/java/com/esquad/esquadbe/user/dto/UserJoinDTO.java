77c5939ff1c52fdc60f1884122c1bea86988311c
jeongmin <jeongmin0046@gmail.com>
2024-09-03 20:56:53 +0900

    mod: userId를 username으로 수정

--- src/main/java/com/esquad/esquadbe/user/dto/UserJoinDTO.java
+++ src/main/java/com/esquad/esquadbe/user/dto/UserJoinDTO.java
@@ -11,6 +11,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,26 +22,26 @@ import java.time.LocalDate;
 @Builder
 @ToString
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class UserJoinDTO {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @NotBlank(message = "아이디를 입력해주세요.")
     @Pattern(regexp = "^[a-zA-Z]{8,12}$", message = "아이디는 영어만 포함한 8~12자여야 합니다.")
-    private String userId;
+    private String username;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
     @Size(min = 8, max = 16, message = "비밀번호는 8~16자여야 합니다.")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?=.*[!~&+@]).*$", message = "비밀번호는 대소문자와 특수문자(!~&+@)를 포함해야 합니다.")
     private String password;
 
-    @NotBlank(message = "이름을 입력해주세요.")
-    private String userName;
-
     @NotBlank(message = "이메일을 입력해주세요.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
 
     @NotBlank(message = "연락처를 입력해주세요.")
-    private String phoneNo;
+    private String phoneNumber;
 
     @NotNull(message = "생년월일을 입력해주세요.")
     private LocalDate birthDay;
@@ -52,16 +53,15 @@ public class UserJoinDTO {
 
 
     public void setDefaultNickname() {
-            this.nickname = this.userId;
+            this.nickname = this.username;
     }
 
     public User toEntity(BCryptPasswordEncoder bCryptPasswordEncoder) {
         return User.builder()
-                .userId(this.userId)
+                .username(this.username)
                 .password(bCryptPasswordEncoder.encode(this.password))
-                .userName(this.userName)
                 .email(this.email)
-                .phoneNo(this.phoneNo)
+                .phoneNumber(this.phoneNumber)
                 .birthDay(this.birthDay)
                 .address(this.address)
                 .nickname(this.nickname)
