d9b4a022de37fcd28d222a1d97197cfe23847ed5
jeongmin <jeongmin0046@gmail.com>
2024-09-03 20:55:32 +0900

    mod: 연관관계 FetchType.Eager 수정

--- src/main/java/com/esquad/esquadbe/user/entity/User.java
+++ src/main/java/com/esquad/esquadbe/user/entity/User.java
@@ -12,6 +12,7 @@ import com.esquad.esquadbe.studypage.entity.StudyPageUser;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
@@ -24,6 +25,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
@@ -37,6 +39,7 @@ import java.util.Set;
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 @Table(name = "USERS")
 public class User extends BasicEntity {
 
@@ -44,56 +47,53 @@ public class User extends BasicEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "USER_ID", nullable = false, unique = true, length = 20)
+    @Column(nullable = false, unique = true, length = 20)
     @Pattern(regexp = "^[a-zA-Z]{8,12}$", message = "아이디는 영어만 포함한 8~12자여야 합니다.")
-    private String userId;
+    private String username;
 
-    @Column(name = "NICKNAME", nullable = false, unique = true, length = 20)
+    @Column(nullable = false, unique = true, length = 20)
     @Size(min = 2, max = 20, message = "닉네임은 2~20자여야 합니다.")
     private String nickname;
 
-    @Column(name = "PASSWORD", nullable = false, length = 60)
-    @Size(min = 8, max = 16, message = "비밀번호는 8~16자여야 합니다.")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?=.*[!~&+@]).*$", message = "비밀번호는 대소문자와 특수문자(!~&+@)를 포함해야 합니다.")
+    @Column(nullable = false, length = 60)
+//    @Size(min = 8, max = 16, message = "비밀번호는 8~16자여야 합니다.")
+//    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?=.*[!~&+@]).*$", message = "비밀번호는 대소문자와 특수문자(!~&+@)를 포함해야 합니다.")
     private String password;
 
-    @Column(name = "USER_NAME", nullable = false, length = 12)
-    private String userName;
-
-    @Column(name = "EMAIL", nullable = false, length = 25)
+    @Column(nullable = false, length = 25)
     private String email;
 
-    @Column(name = "PHONE_NO", nullable = false, length = 12)
-    private String phoneNo;
+    @Column(nullable = false, length = 12)
+    private String phoneNumber;
 
-    @Column(name = "BIRTH_DAY", nullable = false)
+    @Column(nullable = false)
     private LocalDate birthDay;
 
-    @Column(name = "ADDRESS", nullable = false, length = 255)
+    @Column(nullable = false, length = 255)
     private String address;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private List<Notification> notifications = new ArrayList<>();
 
-     @OneToMany(mappedBy = "user")
+     @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
      private List<StoredFile> storedFiles = new ArrayList<>();
 
-    @OneToMany(mappedBy = "writer")
+    @OneToMany(mappedBy = "writer", fetch = FetchType.EAGER)
     private List<BookQnaBoard> bookQnaBoards = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private List<StreamingSession> streamingSessions = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private List<StreamingParticipant> streamingParticipants = new ArrayList<>();
 
     @OneToOne(mappedBy = "user")
     private UserSetting userSetting;
 
-    @OneToMany(mappedBy = "member")
+    @OneToMany(mappedBy = "member", fetch = FetchType.EAGER)
     private List<TeamSpaceUser> teamSpaceUsers = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member")
+    @OneToMany(mappedBy = "member", fetch = FetchType.EAGER)
     private Set<StudyPageUser> studyPageUsers = new HashSet<>();
 
 }
