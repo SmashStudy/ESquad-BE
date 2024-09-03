3242c459e713d219a6167c6d7059c05a36bef1a3
jeongmin <jeongmin0046@gmail.com>
2024-09-03 20:57:20 +0900

    feat: 아이디, 닉네임 중복확인 코드 구현

--- src/main/java/com/esquad/esquadbe/user/controller/UserJoinController.java
+++ src/main/java/com/esquad/esquadbe/user/controller/UserJoinController.java
@@ -2,26 +2,31 @@ package com.esquad.esquadbe.user.controller;
 
 import com.esquad.esquadbe.user.dto.UserJoinDTO;
 import com.esquad.esquadbe.user.service.UserJoinService;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
 
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserJoinController {
 
     private final UserJoinService userJoinService;
 
-    @GetMapping("/join")
-    public String joinPage() {
-        return "join";
-    }
-
-    @PostMapping("/joinProc")
-    public String joinProcess(@RequestBody UserJoinDTO userJoinDTO) {
+    @PostMapping
+    public String joinProcess(@RequestBody @NotNull UserJoinDTO userJoinDTO) {
         try {
             userJoinService.joinProcess(userJoinDTO);
             return "회원가입 성공";
@@ -30,5 +35,19 @@ public class UserJoinController {
         }
     }
 
+    @GetMapping("/usernamecheck")
+    public ResponseEntity<Map<String, Boolean>> checkUsername(@RequestParam String username) {
+        boolean existsByUsername = userJoinService.checkUsername(username);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("available", !existsByUsername);
+        return ResponseEntity.ok(response);
+    }
 
+    @GetMapping("/usernicknamecheck")
+    public ResponseEntity<Map<String, Boolean>> checkNickname(@RequestParam String nickname) {
+        boolean existsByNickname = userJoinService.checkNickname(nickname);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("available", !existsByNickname);
+        return ResponseEntity.ok(response);
+    }
 }
