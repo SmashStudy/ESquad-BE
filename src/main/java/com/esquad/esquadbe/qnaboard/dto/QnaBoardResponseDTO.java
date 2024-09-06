8aa9f1c582e5ef6ee14dace6dcb88f69651f2806
wnsgud0310 <ffjjo0310@naver.com>
2024-09-06 18:45:28 +0900

    mod: QnaBoardResponseDTO 클래스 수정

--- src/main/java/com/esquad/esquadbe/qnaboard/dto/QnaBoardResponseDTO.java
+++ src/main/java/com/esquad/esquadbe/qnaboard/dto/QnaBoardResponseDTO.java
@@ -1,18 +1,34 @@
 package com.esquad.esquadbe.qnaboard.dto;
 
+import com.esquad.esquadbe.qnaboard.entity.BookQnaBoard;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.format.DateTimeFormatter;
+
+@Builder
 @Getter
 @Setter
 public class QnaBoardResponseDTO {
     private Long id;
-    private String writerName;
-    private Long studyPageId;
-    private Long bookId;
     private String title;
+    private String writerName;
+    private String book;
     private String content;
-    private Integer likes;
     private String createdAt;
-    private String updatedAt;
+    private Integer likes;
+
+    // 정적 팩토리 메서드
+    public static QnaBoardResponseDTO from(BookQnaBoard bookQnaBoard) {
+        return QnaBoardResponseDTO.builder()
+                .id(bookQnaBoard.getId())
+                .title(bookQnaBoard.getTitle())
+                .writerName(bookQnaBoard.getWriter())
+                .book(bookQnaBoard.getBook())
+                .content(bookQnaBoard.getContent())
+                .createdAt(bookQnaBoard.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                .likes(bookQnaBoard.getLikes())
+                .build();
+    }
 }
