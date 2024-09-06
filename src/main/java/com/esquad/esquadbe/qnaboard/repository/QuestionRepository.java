36b322d2f7912fdcee9bc0584fbdc2afc887784f
wnsgud0310 <ffjjo0310@naver.com>
2024-09-06 18:46:38 +0900

    mod: QuestionRepository 레포지토리 수정

--- src/main/java/com/esquad/esquadbe/qnaboard/repository/QuestionRepository.java
+++ src/main/java/com/esquad/esquadbe/qnaboard/repository/QuestionRepository.java
@@ -1,12 +1,14 @@
 package com.esquad.esquadbe.qnaboard.repository;
 
-
 import com.esquad.esquadbe.qnaboard.entity.BookQnaBoard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface QuestionRepository extends JpaRepository<BookQnaBoard, Long> {
     Page<BookQnaBoard> findAll(Pageable pageable);
+    // 제목으로 게시글 검색
+    Page<BookQnaBoard> findByTitleContaining(String title, Pageable pageable);
+    // 작성자로 게시글 검색
+    Page<BookQnaBoard> findByWriter(String writer, Pageable pageable);
 }
