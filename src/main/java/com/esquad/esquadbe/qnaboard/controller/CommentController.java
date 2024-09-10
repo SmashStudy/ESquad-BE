5ada5a1abc1895fc644603fe445089305a0f885a
wnsgud0310 <ffjjo0310@naver.com>
2024-09-10 18:38:04 +0900

    add: 댓글 컨트롤러 생성

---
+++ src/main/java/com/esquad/esquadbe/qnaboard/controller/CommentController.java
@@ -0,0 +1,10 @@
+package com.esquad.esquadbe.qnaboard.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class CommentController {
+}
