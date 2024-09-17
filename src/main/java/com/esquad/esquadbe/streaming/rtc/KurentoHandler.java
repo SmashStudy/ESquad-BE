42361ecc8f059c09899447a46a1048d0a576150b
andle7 <hb1809@naver.com>
2024-09-17 22:56:36 +0900

    feat: 처리되지 않은 메시지와 예외 발생 시 에러 메시지 전송을 위한 sendErrorMessage 메서드 구현

--- src/main/java/com/esquad/esquadbe/streaming/rtc/KurentoHandler.java
+++ src/main/java/com/esquad/esquadbe/streaming/rtc/KurentoHandler.java
@@ -1,13 +1,17 @@
 package com.esquad.esquadbe.streaming.rtc;
 
+import com.esquad.esquadbe.streaming.dto.KurentoRoomDto;
 import com.esquad.esquadbe.streaming.service.KurentoManager;
 import com.esquad.esquadbe.streaming.service.KurentoUserRegistry;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import lombok.RequiredArgsConstructor;
 import org.kurento.client.KurentoClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
@@ -19,4 +23,42 @@ public class KurentoHandler extends TextWebSocketHandler {
     private final KurentoManager roomManager;
     private final KurentoClient kurentoClient;
     private final Gson gson = new Gson();
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) {
+        try {
+            JsonObject jsonMessage = gson.fromJson(message.getPayload(), JsonObject.class);
+            String id = jsonMessage.get("id").getAsString();
+
+            KurentoUserSession user = registry.getBySession(session);
+            if (user != null) {
+                log.debug("사용자 '{}'로부터 메시지 수신: {}", user.getUserId(), jsonMessage);
+            } else {
+                log.debug("새로운 사용자로부터 메시지 수신: {}", jsonMessage);
+            }
+
+            switch (id) {
+                case "joinRoom":
+                    //handleJoinRoom(session, jsonMessage);
+                    break;
+                default:
+                    log.warn("세션 '{}'에서 처리되지 않은 메시지 ID: {}", session.getId(), id);
+                    break;
+            }
+        } catch (Exception e) {
+            log.error("세션 '{}'에서 WebSocket 메시지 처리 중 오류 발생", session.getId(), e);
+            sendErrorMessage(session, "메시지 처리 중 오류 발생");
+        }
+    }
+
+    private void sendErrorMessage(WebSocketSession session, String error) {
+        try {
+            JsonObject errorMessage = new JsonObject();
+            errorMessage.addProperty("id", "error");
+            errorMessage.addProperty("message", error);
+            session.sendMessage(new TextMessage(errorMessage.toString()));
+        } catch (Exception e) {
+            log.error("세션 '{}'에 에러 메시지 전송 중 오류 발생: {}", session.getId(), e.getMessage());
+        }
+    }
 }
