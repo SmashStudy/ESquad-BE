1b361e63f07c34a365bb896ec7186321327d8945
andle7 <hb1809@naver.com>
2024-09-17 21:56:35 +0900

    mod: KurentoHandler 클래스와 WebRtcConfig 클래스 수정

--- src/main/java/com/esquad/esquadbe/streaming/config/WebRtcConfig.java
+++ src/main/java/com/esquad/esquadbe/streaming/config/WebRtcConfig.java
@@ -1,15 +1,10 @@
 package com.esquad.esquadbe.streaming.config;
 
-import com.esquad.esquadbe.streaming.rtc.KurentoHandler;
 import com.esquad.esquadbe.streaming.service.KurentoManager;
 import com.esquad.esquadbe.streaming.service.KurentoUserRegistry;
-import org.kurento.client.KurentoClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.net.ssl.*;
 
 @Configuration
 public class WebRtcConfig {
--- src/main/java/com/esquad/esquadbe/streaming/rtc/KurentoHandler.java
+++ src/main/java/com/esquad/esquadbe/streaming/rtc/KurentoHandler.java
@@ -1,8 +1,21 @@
 package com.esquad.esquadbe.streaming.rtc;
 
+import com.esquad.esquadbe.streaming.service.KurentoManager;
+import com.esquad.esquadbe.streaming.service.KurentoUserRegistry;
+import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
+import org.kurento.client.KurentoClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
+@RequiredArgsConstructor
 public class KurentoHandler extends TextWebSocketHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(KurentoHandler.class);
+    private final KurentoUserRegistry registry;
+    private final KurentoManager roomManager;
+    private final Gson gson = new Gson();
 }
