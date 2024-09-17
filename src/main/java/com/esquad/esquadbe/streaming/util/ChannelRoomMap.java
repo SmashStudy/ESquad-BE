f0e2b83f1ce4270872d0c359a239eede8b3c185d
andle7 <hb1809@naver.com>
2024-09-17 21:36:01 +0900

    mod: ChannelRoomMap 클래스 수정

--- src/main/java/com/esquad/esquadbe/streaming/util/ChannelRoomMap.java
+++ src/main/java/com/esquad/esquadbe/streaming/util/ChannelRoomMap.java
@@ -1,4 +1,21 @@
 package com.esquad.esquadbe.streaming.util;
 
+import com.esquad.esquadbe.streaming.dto.KurentoRoomDto;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ChannelRoomMap {
+
+    private static ChannelRoomMap instance = new ChannelRoomMap();
+    private final ConcurrentHashMap<String, KurentoRoomDto> channelRooms = new ConcurrentHashMap<>();
+
+    private ChannelRoomMap() {}
+
+    public static ChannelRoomMap getInstance() {
+        return instance;
+    }
+
+    public ConcurrentHashMap<String, KurentoRoomDto> getChannelRooms() {
+        return channelRooms;
+    }
 }
