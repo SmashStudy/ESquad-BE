757ec9f33f516d076146ca08b9bd506d530a6026
andle7 <hb1809@naver.com>
2024-09-17 21:49:52 +0900

    feat: WebRTC 설정 구성 추가 및 의존성 주입

--- src/main/java/com/esquad/esquadbe/streaming/config/WebRtcConfig.java
+++ src/main/java/com/esquad/esquadbe/streaming/config/WebRtcConfig.java
@@ -1,4 +1,29 @@
 package com.esquad.esquadbe.streaming.config;
 
+import com.esquad.esquadbe.streaming.rtc.KurentoHandler;
+import com.esquad.esquadbe.streaming.service.KurentoManager;
+import com.esquad.esquadbe.streaming.service.KurentoUserRegistry;
+import org.kurento.client.KurentoClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.*;
+
+@Configuration
 public class WebRtcConfig {
+
+    @Value("${kms.url}")
+    private String kmsUrl;
+
+    private final KurentoUserRegistry registry;
+    private final KurentoManager roomManager;
+
+    @Autowired
+    public WebRtcConfig(KurentoUserRegistry registry, KurentoManager roomManager) {
+        this.registry = registry;
+        this.roomManager = roomManager;
+    }
 }
+
