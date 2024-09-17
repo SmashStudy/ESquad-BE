7b8c325868ed3322114d624129b16b94dedf0c81
andle7 <hb1809@naver.com>
2024-09-17 21:39:08 +0900

    mod: KurentoManager 클래스 수정

--- src/main/java/com/esquad/esquadbe/streaming/service/KurentoManager.java
+++ src/main/java/com/esquad/esquadbe/streaming/service/KurentoManager.java
@@ -1,4 +1,14 @@
 package com.esquad.esquadbe.streaming.service;
 
+import com.esquad.esquadbe.streaming.util.ChannelRoomMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KurentoManager {
+
+    private final Logger log = LoggerFactory.getLogger(KurentoManager.class);
+    private final ChannelRoomMap roomMap = ChannelRoomMap.getInstance();
+    private static final int MAX_PARTICIPANTS = 12;
 }
