1559183ec9038d83cf4882e2a8def2eeed3fb58f
andle7 <hb1809@naver.com>
2024-09-17 21:41:10 +0900

    mod: KurentoUserRegistry 클래스 수정

--- src/main/java/com/esquad/esquadbe/streaming/service/KurentoUserRegistry.java
+++ src/main/java/com/esquad/esquadbe/streaming/service/KurentoUserRegistry.java
@@ -1,4 +1,16 @@
 package com.esquad.esquadbe.streaming.service;
 
+import com.esquad.esquadbe.streaming.rtc.KurentoUserSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
 public class KurentoUserRegistry {
+
+    private static final Logger log = LoggerFactory.getLogger(KurentoUserRegistry.class);
+    private final ConcurrentHashMap<String, KurentoUserSession> usersByName = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, KurentoUserSession> usersBySessionId = new ConcurrentHashMap<>();
 }
