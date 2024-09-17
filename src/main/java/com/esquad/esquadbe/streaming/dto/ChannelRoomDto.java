ca3888ad32a2457d7352c1c3220cf8933cb8c861
andle7 <hb1809@naver.com>
2024-09-17 21:26:06 +0900

    mod: ChannelRoomDto 클래스 수정

--- src/main/java/com/esquad/esquadbe/streaming/dto/ChannelRoomDto.java
+++ src/main/java/com/esquad/esquadbe/streaming/dto/ChannelRoomDto.java
@@ -1,4 +1,12 @@
 package com.esquad.esquadbe.streaming.dto;
 
+import lombok.Getter;
+
+@Getter
 public class ChannelRoomDto {
+    private String roomId;
+
+    public ChannelRoomDto(String roomId) {
+        this.roomId = roomId;
+    }
 }
