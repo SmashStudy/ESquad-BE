23984bb9d4161da671aa9b32604688133db0ce57
0237hh <153145663+0237hh@users.noreply.github.com>
2024-09-19 22:41:14 +0900

    add: S3FileController 클래스 생성

---
+++ src/main/java/com/esquad/esquadbe/chat/controller/S3FileController.java
@@ -0,0 +1,24 @@
+package com.esquad.esquadbe.chat.controller;
+
+import com.esquad.esquadbe.chat.service.FileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chat/file")
+public class S3FileController {
+
+    private final FileService fileService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> fileUpload(@RequestPart(value = "file", required = false) MultipartFile multipartFile,
+                                        @RequestParam("username") String username) {
+
+        return fileService.uploadFile(multipartFile, username);
+    }
+}
