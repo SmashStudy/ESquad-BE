9c74155783912c579c1e5e8b9da9307a07ff33ec
0237hh <153145663+0237hh@users.noreply.github.com>
2024-09-19 22:41:59 +0900

    feat: 파일 다운로드 기능 구현

--- src/main/java/com/esquad/esquadbe/chat/controller/S3FileController.java
+++ src/main/java/com/esquad/esquadbe/chat/controller/S3FileController.java
@@ -3,9 +3,15 @@ package com.esquad.esquadbe.chat.controller;
 import com.esquad.esquadbe.chat.service.FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @RestController
@@ -26,5 +32,24 @@ public class S3FileController {
     public ResponseEntity<String> deleteFile(@PathVariable String filename) {
         fileService.deleteFile(filename);
         return ResponseEntity.ok("File deleted successfully with " + filename);
+    }
+
+    @GetMapping ("/download/{filename}")
+    public ResponseEntity<ByteArrayResource> fileDownload(@PathVariable String filename) {
+        try {
+            ResponseEntity<byte[]> data = fileService.downloadFile(filename);
+
+            ByteArrayResource resource = new ByteArrayResource(data.getBody());
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION,
+                            "attachment; filename = " + URLEncoder.encode(
+                                    filename, StandardCharsets.UTF_8))
+                    .body(resource);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
