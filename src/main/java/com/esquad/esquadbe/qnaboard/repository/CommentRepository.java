0db43228b6379305016409f42d50282565937208
wnsgud0310 <ffjjo0310@naver.com>
2024-09-22 23:37:57 +0900

    mod: CommentRepository 댓글 레포지토리 수정

--- src/main/java/com/esquad/esquadbe/qnaboard/repository/CommentRepository.java
+++ src/main/java/com/esquad/esquadbe/qnaboard/repository/CommentRepository.java
@@ -1,2 +1,17 @@
-package com.esquad.esquadbe.qnaboard.repository;public interface CommentRepository {
+package com.esquad.esquadbe.qnaboard.repository;
+
+import com.esquad.esquadbe.qnaboard.entity.BookQnaBoard;
+import com.esquad.esquadbe.qnaboard.entity.BookQnaReply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository  extends JpaRepository<BookQnaReply, Long> {
+
+    // 특정 게시글에 달린 댓글 조회
+    List<BookQnaReply> findByBoard(BookQnaBoard board);
+
+
+
+
 }
