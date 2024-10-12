159d7cfb5aa255735f4f1cb0415281e86f340994
wive10 <70935978+wive10@users.noreply.github.com>
2024-10-13 00:42:25 +0900

    rename: StudyPageDto -> StudyPageCreateDto

--- src/main/java/com/esquad/esquadbe/studypage/dto/StudyPageCreateDto.java
+++ src/main/java/com/esquad/esquadbe/studypage/dto/StudyPageCreateDto.java
@@ -1,22 +1,17 @@
 package com.esquad.esquadbe.studypage.dto;
 
-import com.esquad.esquadbe.studypage.entity.Book;
-import com.esquad.esquadbe.studypage.entity.StudyRemind;
-import com.esquad.esquadbe.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-public class StudyPageDto {
+public class StudyPageCreateDto {
     private BookSearchResultItemDto bookDto;
     private StudyInfoDto studyInfoDto;
     private List<StudyRemindDto> reminds;
