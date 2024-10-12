f8179230ced580517bf46b68e699e02e0f9aaddf
wive10 <70935978+wive10@users.noreply.github.com>
2024-10-13 00:48:38 +0900

    add: delete

--- src/main/java/com/esquad/esquadbe/studypage/repository/StudyRemindRepository.java
+++ src/main/java/com/esquad/esquadbe/studypage/repository/StudyRemindRepository.java
@@ -1,5 +1,6 @@
 package com.esquad.esquadbe.studypage.repository;
 
+import com.esquad.esquadbe.studypage.entity.StudyPage;
 import com.esquad.esquadbe.studypage.entity.StudyRemind;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface StudyRemindRepository extends CrudRepository< StudyRemind, Long> {
+public interface StudyRemindRepository extends CrudRepository<StudyRemind, Long> {
 
+    void deleteByStudyPage(StudyPage studyPage);
 }
