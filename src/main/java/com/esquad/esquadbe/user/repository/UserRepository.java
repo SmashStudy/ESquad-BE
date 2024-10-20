54f3b064fc19750d1c6b83335a512b076efd7571
wive10 <70935978+wive10@users.noreply.github.com>
2024-10-20 15:10:01 +0900

    mod: user repository 리젝트

--- src/main/java/com/esquad/esquadbe/user/repository/UserRepository.java
+++ src/main/java/com/esquad/esquadbe/user/repository/UserRepository.java
@@ -3,18 +3,14 @@ package com.esquad.esquadbe.user.repository;
 import com.esquad.esquadbe.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     boolean existsByNickname(String nickname);
-
-    User findByUsername(String username);
-    User findById(long id);
-
-    Iterable<User> findAll();
-
-
+    Optional<User> findById(long id);
     Optional<User> findByUsername(String username);
+    List<User> findAll();
 
 }
