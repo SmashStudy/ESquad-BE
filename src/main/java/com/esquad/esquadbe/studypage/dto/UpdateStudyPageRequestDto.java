a2fe642a2eaf5a7beb840176bf1a5e251893ef7c
wive10 <70935978+wive10@users.noreply.github.com>
2024-10-20 15:12:43 +0900

    mod: 일단 권한 애

--- src/main/java/com/esquad/esquadbe/studypage/dto/UpdateStudyPageRequestDto.java
+++ src/main/java/com/esquad/esquadbe/studypage/dto/UpdateStudyPageRequestDto.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class UpdateStudyPageRequestDto {
-    private Long userId; // 수정 요청한 유저의 ID
     private String title;
     private String description;
     private LocalDate startDate;
