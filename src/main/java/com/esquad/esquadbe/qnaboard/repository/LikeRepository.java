4f1a009b7695493d0a21b0fd1ef5cd32c856cdae
wnsgud0310 <ffjjo0310@naver.com>
2024-10-22 15:55:44 +0900

    add: LikeRepository 생성

---
+++ src/main/java/com/esquad/esquadbe/qnaboard/repository/LikeRepository.java
@@ -0,0 +1,17 @@
+package com.esquad.esquadbe.qnaboard.repository;
+
+import com.esquad.esquadbe.qnaboard.entity.BookQnaLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LikeRepository extends JpaRepository<BookQnaLike, Long> {
+
+    // 특정 사용자가 특정 게시물에 좋아요를 눌렀는지 확인
+    Optional<BookQnaLike> findByUserIdAndBoardId(Long userId, Long boardId);
+
+    // 특정 게시물의 좋아요 수 반환
+    Long countByBoardId(Long boardId);
+}
