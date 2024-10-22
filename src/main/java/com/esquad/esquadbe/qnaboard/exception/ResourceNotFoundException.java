11edd5790069082ab5c3c8d49e5608a822ea29fe
wnsgud0310 <ffjjo0310@naver.com>
2024-10-22 14:02:43 +0900

    mod: qnaboard Exception 파일 경로 수정

---
+++ src/main/java/com/esquad/esquadbe/qnaboard/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.esquad.esquadbe.qnaboard.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
