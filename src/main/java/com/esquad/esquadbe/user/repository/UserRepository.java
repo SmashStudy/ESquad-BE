982727875381e1f6e1d7e0703cd852d9fd44b8c9
jeongmin <jeongmin0046@gmail.com>
2024-10-22 16:33:42 +0900

    mod: findAll 코드 삭제

--- src/main/java/com/esquad/esquadbe/user/repository/UserRepository.java
+++ src/main/java/com/esquad/esquadbe/user/repository/UserRepository.java
@@ -14,7 +14,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
    Optional<User> findById(long id);
    Optional<User> findByUsername(String username);
    Optional<User> findByNickname(String nickname);
-   List<User> findAll();
    User findByEmail(String email);
    User findByEmailAndUsername(String email, String username);
 }
