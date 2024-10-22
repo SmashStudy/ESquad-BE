cde09d736d5eb49cf3414c80940591f9eb48fb09
jeongmin <jeongmin0046@gmail.com>
2024-10-22 10:39:38 +0900

    remove: 파일 삭제

--- src/main/java/com/esquad/esquadbe/global/exception/custom/user/UserNicknameException.java
+++
@@ -1,8 +0,0 @@
-package com.esquad.esquadbe.global.exception.custom.user;
-
-import com.esquad.esquadbe.global.exception.custom.RestApiException;
-import com.esquad.esquadbe.global.exception.response.UserErrorCode;
-
-public class UserNicknameException extends RestApiException {
-    public UserNicknameException() { super(UserErrorCode.USER_NOT_FOUND_ERROR);}
-}
