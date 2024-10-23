70a4fc233503bb27172957ef69af16c92dc6c7af
wnsgud0310 <ffjjo0310@naver.com>
2024-10-23 11:58:46 +0900

    mod: QnaBoardController 수정

--- src/main/java/com/esquad/esquadbe/qnaboard/controller/QnaBoardController.java
+++ src/main/java/com/esquad/esquadbe/qnaboard/controller/QnaBoardController.java
@@ -21,7 +21,7 @@ public class QnaBoardController {
 
     private final QuestionService questionService;
 
-
+    // 전체 게시글 조회
     @GetMapping
     public Page<QnaBoardResponseDTO> getAllQuestions(
             @RequestParam(defaultValue = "0") int page,
@@ -29,13 +29,13 @@ public class QnaBoardController {
         return questionService.getAllQuestions(page, size);
     }
 
-
+    // 특정 ID의 게시글 조회 (게시글번호)
     @GetMapping("/{id}")
     public QnaBoardResponseDTO getQuestionById(@PathVariable Long id) {
         return questionService.getQuestionById(id);
     }
 
-
+    // 새로운 질문 생성
     @PostMapping
     public QnaBoardResponseDTO createQuestion(
             @RequestBody QnaRequestDTO qnaForm,
@@ -44,7 +44,7 @@ public class QnaBoardController {
     }
 
 
-
+    // 특정 제목의 게시글 조회
     @GetMapping("/search")
     public Page<QnaBoardResponseDTO> getQuestionsByTitle(
             @RequestParam("title") String title,
@@ -53,7 +53,7 @@ public class QnaBoardController {
         return questionService.getQuestionsByTitle(title, page, size);
     }
 
-
+    // 특정 작성자의 게시글 조회
     @GetMapping("/by-writer")
     public Page<QnaBoardResponseDTO> getQuestionsByWriter(
             Principal principal,
@@ -62,18 +62,18 @@ public class QnaBoardController {
         return questionService.getQuestionsByWriter(principal.getName(), page, size);
     }
 
-
+    // 게시글 수정
     @PutMapping("/{id}")
-    public QnaBoardResponseDTO updateQuestion(@PathVariable Long id, @RequestBody QnaRequestDTO updateForm, Principal principal) {
-        return questionService.updateQuestion(id, updateForm, principal);
+    public QnaBoardResponseDTO updateQuestion(@PathVariable Long id, @RequestBody QnaRequestDTO updateForm) {
+        return questionService.updateQuestion(id, updateForm);
     }
 
 
-
+    // 게시글 삭제
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteQuestion(@PathVariable Long id, Principal principal) {
-
-        questionService.deleteQuestion(id, principal);
+        // 현재 로그인한 사용자의 ID를 서비스로 전달
+        questionService.deleteQuestion(id, principal.getName());
         return ResponseEntity.ok("게시글이 삭제되었습니다");
     }
 }
