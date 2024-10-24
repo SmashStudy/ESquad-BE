1ce5671ac7eb1f9a7587f0d7b1cd39f107f8a9ef
wnsgud0310 <ffjjo0310@naver.com>
2024-10-24 12:09:47 +0900

    remove: 안쓰는 예외클래스 제거

--- src/main/java/com/esquad/esquadbe/qnaboard/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.esquad.esquadbe.qnaboard.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
